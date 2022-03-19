24f0efc1fb9bee61a3ca77caa050cdc463ef8b9b
Newwoc <98651044+Newwoc@users.noreply.github.com>
2022-03-19 03:01:16 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,60 +1,77 @@
+from posixpath import split
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from array import *
+import re
 
-
-quote_page = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=1&n=48'
-page = urlopen(quote_page)
-soup = BeautifulSoup(page, 'html.parser')
-
-
-#Scrape shop pages URLs and save in array
+#Scrape 'trainingsbooster' Category pages and saves every article URL in array
+categoryPage = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=1&n=48')
+soup = BeautifulSoup(categoryPage, 'html.parser')
 urls = [i['href'] for i in soup.find_all("a", href=True, class_="buybox--button")]
-quote_page2 = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=2&n=48'
-page2 = urlopen(quote_page2)
-soup2 = BeautifulSoup(page2, 'html.parser')
+
+categoryPage2 = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=2&n=48')
+soup2 = BeautifulSoup(categoryPage2, 'html.parser')
 for i in soup2.find_all("a", href=True, class_="buybox--button"):
     urls.append(i['href'])
-quote_page3 = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=3&n=48'
-page3 = urlopen(quote_page3)
-soup3 = BeautifulSoup(page3, 'html.parser')
+
+categoryPage3 = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=3&n=48')
+soup3 = BeautifulSoup(categoryPage3, 'html.parser')
 for i in soup3.find_all("a", href=True, class_="buybox--button"):
     urls.append(i['href'])
-quote_page4 = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=4&n=48'
-page4 = urlopen(quote_page4)
-soup4 = BeautifulSoup(page4, 'html.parser')
+
+categoryPage4 = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=4&n=48')
+soup4 = BeautifulSoup(categoryPage4, 'html.parser')
 for i in soup4.find_all("a", href=True, class_="buybox--button"):
     urls.append(i['href'])
-quote_page5 = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=5&n=48'
-page5 = urlopen(quote_page5)
-soup5 = BeautifulSoup(page5, 'html.parser')
+
+categoryPage5 = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=5&n=48')
+soup5 = BeautifulSoup(categoryPage5, 'html.parser')
 for i in soup5.find_all("a", href=True, class_="buybox--button"):
     urls.append(i['href'])
-quote_page6 = 'https://www.stayfocused.de/supplements/trainingsbooster/?p=6&n=48'
-page6 = urlopen(quote_page6)
-soup6 = BeautifulSoup(page6, 'html.parser')
+
+categoryPage6 = urlopen('https://www.stayfocused.de/supplements/trainingsbooster/?p=6&n=48')
+soup6 = BeautifulSoup(categoryPage6, 'html.parser')
 for i in soup6.find_all("a", href=True, class_="buybox--button"):
     urls.append(i['href'])
 
 
-
+#define arrays
 productUrl=[]
 productTitle=[]
-productInhaltsstoffe=[]
-#Scrape productpage for Title, URLs 
+productPrice=[]
+productDescription=[]
+productContents=[]
+productPictureUrl=[]
+
+#Scrape productpage for Product title, price and description 
 for i in urls:
     productPage = i
     openProductPage=urlopen(productPage)
-    productSoup=BeautifulSoup(openProductPage, 'html.parser')
-    productTitle.append(productSoup.find('h1', {'class' : 'product--title'}))
-    #productURL.append(i)
-    #productInhaltsstoffe.append()
+    parsedProductPage = BeautifulSoup(openProductPage, 'html.parser')
+    productTitle.append(parsedProductPage.find('h1', {'class' : 'product--title'}))
+    productPrice.append(parsedProductPage.find('span', {'class' : 'price--content'}))
+    productDescription.append(parsedProductPage.find('div', {'class' : 'product--description'}))
+    productContents.append(parsedProductPage.find('div', {'class' : 'price--unit'}))
+    productPictureUrl.append(parsedProductPage.find('span', {'class' : 'image--element'}))
+             
+
 
 for i in productTitle:    
-    print(i.text.strip()) #strip text between elements
+    print(i.text) #strips html text between elements and prints product title
 
 for i in urls:    
     print(i)    
 
+for i in productPrice:
+    print(i.text) 
+
+for i in productDescription:
+    print(i.text) 
+
+for i in productContents:
+    print(i.text)
     
+for i in productPictureUrl:
+    print((i['data-img-small'])) #strips image from tag
+    print((i['data-img-large']))
+
     
